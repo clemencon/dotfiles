8ab0d3396802bbc7d1d9d1d9d4a739207cb6be31
Julien Clémençon <julien@clemencon.dev>
2023-06-10 08:07:08 +0200

    Add a floating terminal

--- nvim/lua/clemencon/plugins.lua
+++ nvim/lua/clemencon/plugins.lua
@@ -208,6 +208,21 @@ use({
   end,
 })
 
+--- Floating terminal.
+use({
+  'voldikss/vim-floaterm',
+  config = function()
+    vim.g.floaterm_width = 0.8
+    vim.g.floaterm_height = 0.8
+    vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+    vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+    vim.cmd([[
+      highlight link Floaterm CursorLine
+      highlight link FloatermBorder CursorLineBg
+    ]])
+  end
+})
+
 -- Git commands.
 use({
   'tpope/vim-fugitive',
