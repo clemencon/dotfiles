1d336542a46187c99576a37a6b51a44f63c7ca80
Julien Clémençon <julien@clemencon.dev>
2023-06-19 20:50:04 +0200

    Fix the nvim config removal

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ mkdir -p $HOME/.local/bin
 rm $HOME/.tmux.conf
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 
-## Todo: Check out this t script.
-rm $HOME/.local/bin/t
-ln -s $DOTFILES/scripts/t $HOME/.local/bin/t
+# Todo: Check out this t script.
+# rm $HOME/.local/bin/t
+# ln -s $DOTFILES/scripts/t $HOME/.local/bin/t
 
-rm $HOME/.config/nvim
+rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
 
